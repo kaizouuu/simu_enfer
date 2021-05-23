afa95343ee573a01df91f362a8f747795e48e164
obalise <obalie302@gmail.com>
2021-05-23 11:55:29 +0200

    lol

---
+++ cmake-build-debug/CMakeFiles/simu_enfer.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/simu_enfer.dir/fonctions_damnes.c.o"
+  "CMakeFiles/simu_enfer.dir/simu_enfer.c.o"
+  "CMakeFiles/simu_enfer.dir/simulateur.c.o"
+  "CMakeFiles/simu_enfer.dir/torture.c.o"
+  "simu_enfer"
+  "simu_enfer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/simu_enfer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
